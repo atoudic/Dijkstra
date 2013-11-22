139ceb7c1f17524d29ca2c182cc6852695e7eb65
Alexandre Toudic <atoudic@centrale-marseille.fr>
2013-11-22 10:53:39 +0100

    throw the API's bases

---
+++ src/main/java/fr/dijkstra/util/Edge.java
@@ -0,0 +1,20 @@
+package fr.dijkstra.util;
+
+public class Edge {
+    private Vertex target;
+
+    private int distance;
+
+    public Edge(Vertex target, int distance) {
+        this.target = target;
+        this.distance = distance;
+    }
+
+    public Vertex getTarget() {
+        return target;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
